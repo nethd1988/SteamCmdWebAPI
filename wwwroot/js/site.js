182d5762a4c268abdb15302bb6dd6127687e1696
nethd1988 <ngodung1988@gmail.com>
2025-04-16 22:47:23 +0700

    GALUOCDONE

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,13 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener('DOMContentLoaded', function () {
+    console.log('Site JS loaded successfully');
 
-// Write your JavaScript code.
+    // Các chức năng toàn cục
+    function handleAjaxErrors() {
+        $(document).ajaxError(function (event, jqXHR, settings, thrownError) {
+            console.error('AJAX Error:', thrownError);
+            alert('Có lỗi xảy ra: ' + thrownError);
+        });
+    }
+
+    handleAjaxErrors();
+});
