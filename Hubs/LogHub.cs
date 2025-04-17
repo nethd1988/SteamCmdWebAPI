ff7a66a7e5908deaea708cb1fc73bf8b5625adc5
nethd1988 <ngodung1988@gmail.com>
2025-04-17 13:30:31 +0700

    GALUOCDONE

--- Hubs/LogHub.cs
+++ Hubs/LogHub.cs
@@ -7,10 +7,8 @@ namespace SteamCmdWebAPI.Hubs
 {
     public class LogHub : Hub
     {
-        // Dictionary để lưu trữ TaskCompletionSource cho từng profileId
         private static readonly ConcurrentDictionary<int, TaskCompletionSource<string>> _twoFactorCodeTasks = new ConcurrentDictionary<int, TaskCompletionSource<string>>();
 
-        // Phương thức để client gửi mã 2FA về server
         public Task SubmitTwoFactorCode(int profileId, string twoFactorCode)
         {
             if (_twoFactorCodeTasks.TryRemove(profileId, out var tcs))
@@ -20,22 +18,16 @@ namespace SteamCmdWebAPI.Hubs
             return Task.CompletedTask;
         }
 
-        // Phương thức để server yêu cầu mã 2FA từ client
-        // Phương thức để server yêu cầu mã 2FA từ client
         public static async Task<string> RequestTwoFactorCode(int profileId, IHubContext<LogHub> hubContext)
         {
             var tcs = new TaskCompletionSource<string>();
-            _twoFactorCodeTasks.TryRemove(profileId, out _); // Xóa task cũ nếu có
+            _twoFactorCodeTasks.TryRemove(profileId, out _);
             _twoFactorCodeTasks.TryAdd(profileId, tcs);
 
-            // Gửi yêu cầu trực tiếp
             await hubContext.Clients.All.SendAsync("RequestTwoFactorCode", profileId);
-
-            // Đánh dấu lại để sử dụng như cờ báo
             await hubContext.Clients.All.SendAsync("ReceiveLog", $"STEAMGUARD_REQUEST_{profileId}");
 
-            // Giảm thời gian chờ
-            var timeoutTask = Task.Delay(TimeSpan.FromSeconds(45)); // 45 giây timeout
+            var timeoutTask = Task.Delay(TimeSpan.FromSeconds(120));
             var completedTask = await Task.WhenAny(tcs.Task, timeoutTask);
 
             if (completedTask == timeoutTask)
@@ -45,6 +37,15 @@ namespace SteamCmdWebAPI.Hubs
             }
 
             return await tcs.Task;
+        }
+
+        public Task CancelTwoFactorRequest(int profileId)
+        {
+            if (_twoFactorCodeTasks.TryRemove(profileId, out var tcs))
+            {
+                tcs.SetResult(string.Empty);
+            }
+            return Clients.All.SendAsync("CancelTwoFactorRequest", profileId);
         }
     }
 }
